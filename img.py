a22df63a36ed933ac30327a2d5b2852800498031
Inspiring26 <1002990258@qq.com>
2017-03-30 16:26:09 +0800

    2017年 3月30日 星期四 16时26分02秒 CST

--- img.py
+++ img.py
@@ -2,6 +2,11 @@ import cv2
 color_img=cv2.imread("330.jpg")
 gray_img=cv2.imread("330.jpg",cv2.IMREAD_GRAYSCALE)
 cv2.namedWindow("image")
+cv2.imshow("image",color_img)
+cv2.waitKey(5000)
+cv2.destroyAllWindows()
+
+
 cv2.imshow("image",gray_img)
-cv2.waitKey(10000)
+cv2.waitKey(5000)
 cv2.destroyAllWindows()
