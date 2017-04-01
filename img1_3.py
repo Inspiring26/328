9a2082b1d7220e2bdb6821f8e3c5ab1bac3e8c86
Inspiring26 <1002990258@qq.com>
2017-04-01 10:38:55 +0800

    2017年 4月 1日 星期六 10时38分44秒 CST

---
+++ img1_3.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+out = cv2.VideoWriter('out.avi',-1, 20.0, (640,480))
+while(cap.isOpened()):
+        ret, frame = cap.read()
+        frame = cv2.flip(frame,0)
+        out.write(frame)
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+cap.release()
+out.release()
+cv2.destroyAllWindows()
